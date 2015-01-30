a201fc0e8fce20e2241d3947d88ebf8f8a170565
Aden-Xin <adenxin@gmail.com>
2015-01-29 17:23:04 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ function Character(stats){
   this.maxmp = this.intelligence*3;
   this.name = stats.name;
 }
+
+function Battle(){
+  <a href='GO.html'>Battle<a>
+  <a href='No.html'>Run<a>
+}
+
+function Enemy(){
+  var Enemy = new Character({name: Name, toughness: Tou, strength: Str, intelligence: Int, agility: Agi});
+}
+
+function Enemy2(){
+  var Enemy2 = new Character({name: Name, toughness: Tou, strength: Str, intelligence: Int, agility: Agi});
+}
+
+function Enemy3(){
+  var Enemy3 = new Character({name: Name, toughness: Tou, strength: Str, intelligence: Int, agility: Agi});
+}
