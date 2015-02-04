47dc02834e0647e3d670886b646290a72eefd8d6
Aden-Xin <adenxin@gmail.com>
2015-02-03 17:25:33 -0800

    Update pass.js

--- pass.js
+++ pass.js
@@ -1,10 +1,4 @@
 function Password(){
   var person = prompt("Password", "");
-  var password;
-  var code;
-  var passcode;
-  
-  if (person == $ra11oD){
-    
-  }
+  document.getElementById("A");
 }
